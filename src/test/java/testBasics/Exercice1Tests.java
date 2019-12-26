f5b0475091482c882041fd7ff0b388e19f1b1576
DADYSoukaina <dadysoukaina@gmail.com>
2019-12-26 16:44:25 +0100

    Add unitary tests using JUnit5

---
+++ src/test/java/testBasics/Exercice1Tests.java
@@ -0,0 +1,54 @@
+package testBasics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import basics.Exercice1;
+
+class Exercice1Tests {
+
+	Exercice1 ex = new Exercice1();
+
+	@BeforeEach
+	void beforEach(TestInfo info) {
+		System.out.println("Start test case for " + info.getDisplayName());
+	}
+
+	@Test
+	void additionTest() {
+		int actualResult = ex.calculate(7, "+", 8);
+		int expectedResult = 15;
+		assertEquals(actualResult, expectedResult);
+	}
+
+	@Test
+	void substractionTest() {
+		int actualResult = ex.calculate(19, "-", 30);
+		int expectedResult = -11;
+		assertEquals(actualResult, expectedResult);
+	}
+
+	@Test
+	void mutiplicationTest() {
+		int actualResult = ex.calculate(7, "*", 5);
+		int expectedResult = 35;
+		assertEquals(actualResult, expectedResult);
+	}
+
+	@Test
+	void divisionTest() {
+		int actualResult = ex.calculate(19, "/", 3);
+		int expectedResult = 6;
+		assertEquals(actualResult, expectedResult);
+	}
+	
+	@AfterEach
+	void afterEach(TestInfo info) {
+		System.out.println("End test case for " + info.getDisplayName());
+	}
+
+}
